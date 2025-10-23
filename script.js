2a96ab2f53da9664c0530c59cf40b92430b01c63
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2025-10-23 19:47:54 +0530

    Layout Deployment

---
+++ script.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+}, { threshold: 0.2 });
+
+const elements = document.querySelectorAll('.scroll-animate');
+elements.forEach(el => observer.observe(el));
+
+
